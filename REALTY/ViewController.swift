4b5a9446dfd6fc2d737e83d4a0675e8948f65b8c
nguyenkhoa <nmkhoa89@gmail.com>
2017-12-06 13:42:03 +0700

    Initial Commit

---
+++ REALTY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  REALTY
+//
+//  Created by Nguyễn Khoa on 12/6/17.
+//  Copyright © 2017 Nguyễn Khoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
